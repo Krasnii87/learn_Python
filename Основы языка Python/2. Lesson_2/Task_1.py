511deb72626f11fae1b99ee26601c4ec7607ccf4
Krasnii87 <krasnii_alex@mail.ru>
2022-03-27 13:39:28 +0300

    (upd) Добавил новый урок

---
+++ Основы языка Python/2. Lesson_2/Task_1.py
@@ -0,0 +1,98 @@
+# a = bin(-4)
+# print(a)
+
+# print(bin(4))
+# print(4 << 6)
+# print(bin(256))
+# print(4 >> 6)
+
+# print(type(4))
+# print(type(4.345))
+# print(complex(3, 5))    # Комплексные числа
+#
+# s1 = 'privet'
+# s2 = ' student'
+# print(s1 + s2)
+# print(s1[::-1])
+
+# print(s1.islower())
+
+# a = [1, 2, 3, 4, 0.31]
+# a.append(5)
+# print(a)
+# a.insert(1, 3)
+# print(a)
+# a.remove(1)
+# print(a)
+# a.remove(3)
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+
+# kortage = (1, 3, 5, 7)
+# print(kortage)
+
+# spisok = list((1, 2, 3, 4, 5))
+# mnojestvo = set('asdf')
+# print(mnojestvo)
+# mnojestvo.add('stroka')
+# print(mnojestvo)
+# frozen_mnojestvo = frozenset('asdf')
+# mnojestvo.pop()
+# print(list(mnojestvo))
+
+# capitals = {'Russia': 'Moskow', 'Germany': 'Berlin'}
+# capitals_2 = dict(key_1='val_1', key_2='val_2')
+# print(capitals)
+# print(capitals_2)
+# print(capitals.keys())
+# print(capitals.values())
+# print(capitals.items())
+# print(capitals.get('Russia'))
+# print(capitals.setdefault('France'))
+# print(capitals)
+# capitals.update(capitals_2)
+# print(capitals)
+
+# print(b'text')
+# print('text'.encode('utf-8'))
+# print(bytes([10, 20, 30, 40, 50]))
+
+# a = bytearray(b'text text2 text3')
+# print(a[0])
+
+# a = None
+# print(a)
+
+# try:
+#     print(500/0)
+# except ZeroDivisionError:
+#     print('Нельзя делить на ноль!')
+
+# spisok = [1, 2, 3, 4, 5, 6]
+# for i in spisok:
+#     print(i)
+
+# spisok = [1,2,3]
+# for i in spisok:
+#     print(i * 2)
+
+# a = 4
+# b = 5
+# print(a/b if b != 0 else 0)
+
+# a = 5
+# b = 4
+# print(a is b)
+
+# my_list = [[10,20,30], [40,50]]
+# print(sum(my_list, []))
+
+# a = 2
+# b = 3
+# a, b = b, a
+# print(a, b)
+
+# my_list = [1,2,3,1,4,5,5,6,7,8,8,9,4,5,6]
+# print(max(set(my_list), key=my_list.count))
+
